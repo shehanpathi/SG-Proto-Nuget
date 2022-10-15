bc67df25f138f9419b168deef123f121ee811625
shehan <shehan@gmail.com>
2022-10-15 20:24:49 +0530

    Add project files.

---
+++ SG-Proto-Nuget/Customer.cs
@@ -0,0 +1,20 @@
+﻿using ProtoBuf;
+using System;
+
+namespace SG_Models
+{
+    [ProtoContract]
+    public class Customer
+    {
+        [ProtoMember(1)]
+        public int customerid { get; set; }
+        [ProtoMember(2)]
+        public string firstName { get; set; }
+        [ProtoMember(3)]
+        public string lastName { get; set; }
+        [ProtoMember(4)]
+        public string email { get; set; }
+    }
+
+
+}
---
+++ SG-Proto-Nuget/Protobuff.cs
@@ -0,0 +1,18 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SG_Models
+{
+    public class ProtoBuff
+    {
+        public static byte[] ProtoSerialize<T>(T record) where T : class
+        {
+            using var stream = new MemoryStream();
+            Serializer.Serialize(stream, record);
+            return stream.ToArray();
+        }
+    }
+}
